752e0c33716920ad3788e43f7456baeedbb722bd
Manfred Merayo <manfredmerayo@gmail.com>
2026-07-13 22:57:44 -0600

    Updates in Ejercicios #1

--- Python Intermedio/Ejercicios de Decoradores/Ejercicios Decoradores #1.py
+++ Python Intermedio/Ejercicios de Decoradores/Ejercicios Decoradores #1.py
@@ -6,15 +6,21 @@ class Product:
 
 def register_IVA(func):
     def wrapper(*args, **kwargs):
+
+        print(f"Parámetros recibidos posicionales (*args): {args}")
+        print(f"Parámetros recibidos por nombre (**kwargs): {kwargs}")
             
         result = func(*args, **kwargs)
+
+        print(f"Valor de retorno de la función: {result}")
 
         return result
     return wrapper
 
 @register_IVA
-def calculate_IVA(product):
-    print(product.price * 0.13)
+def calculate_IVA(product, iva_rate = 0.13):
+    return product.price * iva_rate
 
 my_product = Product(150)
 calculate_IVA(my_product)
+calculate_IVA(my_product, iva_rate = 0.15)
